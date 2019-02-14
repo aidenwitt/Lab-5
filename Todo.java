36f7c05d27b77a07c2b27b685e79426fa0b9d5d4
aidenwitt <47369128+aidenwitt@users.noreply.github.com>
2019-02-14 15:20:54 -0600

    Add files via upload

---
+++ Todo.java
@@ -0,0 +1,37 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-14
+OU ID_______________: 113472850
+OU EMAIL____________: aidenwitt@ou.edu
+
+Tasks:
+1. complete calculateTwoTokens() method
+2. complete calculateThreeTokens() method
+3. complete execute() method
+4. complete parseAndExecute() method
+5. complete tests involving the calculateTwoTokens() method
+6. complete tests involving the calculateThreeTokens() method
+7. complete tests involving the execute() method
+8. complete tests involving the parseAndExecute() method
+
+Estimated Time (minutes):
+1. 10
+2. 10
+3. 10
+4. 10
+5. 15
+6. 15
+7. 15
+8. 15
+
+Actual Time (minutes):
+1. 5
+2. 5
+3. 15
+4. 7
+5. 20
+6. 5
+7. 10
+8. 15
+
+*/}
